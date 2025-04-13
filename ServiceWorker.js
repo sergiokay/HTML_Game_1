063aa1b424b33456c0a5926e80bb9295a09e4bf3
sergiokay <63217244+sergiokay@users.noreply.github.com>
2025-04-13 11:47:21 -0600

    Ult

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Kaykesoft-Maths-0.1";
+const contentToCache = [
+    "Build/Mathsum.loader.js",
+    "Build/Mathsum.framework.js",
+    "Build/Mathsum.data",
+    "Build/Mathsum.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
